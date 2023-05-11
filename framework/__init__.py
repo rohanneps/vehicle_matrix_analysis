5f607963c0f7def1d854e6fc4ad4a703345d84d2
rmamatya <rohan.amatya@idp.com>
2023-05-11 11:08:06 +1000

    made the package pip installable

--- framework/__init__.py
+++
@@ -1,2 +0,0 @@
-from .utility import length
-from .VehicleDataManager import VehicleDataManager
--- framework/utility.py
+++
@@ -1,8 +0,0 @@
-from numpy import ndarray
-
-
-def length(trajectory: ndarray) -> int:
-    """
-    return the length of the trajectory
-    """
-    return len(trajectory)
