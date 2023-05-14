396b28315297ecc79f181adbaabf054a8f24d89a
rmamatya <rohan.amatya@idp.com>
2023-05-14 13:38:08 +1000

    added logging for the framework

--- src/framework/VehicleDataManager.py
+++ src/framework/VehicleDataManager.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from typing import Any, Callable, Union
 from matplotlib import pyplot as plt
 import numpy as np
@@ -10,20 +11,32 @@ class VehicleDataManager:
     """
     Handle operations on vehicle data
     """
+    ROOT_DIR = "."
+    # logging
+    LOG_FILE_NAME: str = os.path.join(ROOT_DIR, "process.log")
+    LOG_FILE_LOG_LEVEL: int = logging.DEBUG
+    CONSOLE_LOG_LEVEL: int = logging.INFO
 
-    PLOT_FIGURE_FILE_NAME: str = os.path.join(".", "plot.png")
+    # plot
+    PLOT_FIGURE_FILE_NAME: str = os.path.join(ROOT_DIR, "plot.png")
+
+    # processing
     DIM_INDEX: int = 0
     DIM_OBJECT_ID: int = 1
     DIM_LATITUDE: int = 2
     DIM_LONGITUDE: int = 3
     OBJECT_ID_OCCURANCE_FILTER_THRESHOLD: int = 2
 
-    def catch_exception(func):
+    def log_and_catch_exception(func) -> Any:
         def wrapper(self, *args, **kwargs):
+            self._logger.debug(f"enter {func.__name__}")
+            output = None
             try:
-                return func(self, *args, **kwargs)
+                output = func(self, *args, **kwargs)
             except Exception as e:  # work on python 3.x
-                print(e)
+                self._logger.error(e)
+            self._logger.debug(f"exit {func.__name__}")
+            return output
 
         return wrapper
 
@@ -34,11 +47,13 @@ class VehicleDataManager:
         self._has_error: bool = False
         self._np_data: ndarray
 
+        self._init_logging()
         self._validate_file_path()
-        self._load_file_content()
-        self._preprocess()
+        if not self._has_error:
+            self._load_file_content()
+            self._preprocess()
 
-    @catch_exception
+    @log_and_catch_exception
     def filter_by_id(self, object_id: Union[int, str]) -> ndarray:
         """
         Return a subset of data for the given id
@@ -54,7 +69,7 @@ class VehicleDataManager:
         ]
         return self._filtered_segment
 
-    @catch_exception
+    @log_and_catch_exception
     def filter(self, func: Callable) -> Any:
         """
         Return the output of callable on filtered segment
@@ -62,7 +77,7 @@ class VehicleDataManager:
         self._validate_filtered_segment()
         return func(self._filtered_segment)
 
-    @catch_exception
+    @log_and_catch_exception
     def plot(self, trajectory: ndarray) -> None:
         """
         plot and save the trajectory
@@ -74,24 +89,41 @@ class VehicleDataManager:
         plt.ylabel("LONGITUDE")
         plt.plot(trajectory[:, self.DIM_LATITUDE], trajectory[:, self.DIM_LONGITUDE])
         plt.savefig(self.PLOT_FIGURE_FILE_NAME)
-        print(f"Plot has been saved to {self.PLOT_FIGURE_FILE_NAME}")
+        self._logger.info(f"Plot has been saved to {self.PLOT_FIGURE_FILE_NAME}")
 
-    @catch_exception
+    def _init_logging(self) -> None:
+        """
+        setup logging configuration
+        """
+        self._logger = logging.getLogger(__name__)
+        self._logger.propagate = False
+        self._logger.setLevel(self.LOG_FILE_LOG_LEVEL)
+
+        c_handler = logging.StreamHandler()
+        f_handler = logging.FileHandler(self.LOG_FILE_NAME)
+        c_handler.setLevel(self.CONSOLE_LOG_LEVEL)
+        f_handler.setLevel(self.LOG_FILE_LOG_LEVEL)
+
+        c_format = logging.Formatter("%(levelname)s: %(message)s")
+        f_format = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
+        c_handler.setFormatter(c_format)
+        f_handler.setFormatter(f_format)
+
+        self._logger.addHandler(c_handler)
+        self._logger.addHandler(f_handler)
+
+    @log_and_catch_exception
     def _load_file_content(self) -> None:
         """
         load file content
         """
-        if not self._has_error:
-            self._np_data = np.load(self._file_path)
+        self._np_data = np.load(self._file_path)
 
-    @catch_exception
+    @log_and_catch_exception
     def _preprocess(self):
         """
         sort the numpy data in timestamp index id and filter unique object_ids
         """
-        if self._has_error:
-            return
-
         # sort index: 1) Sort by index 2) arrange object_id, lat, long by the sorted index
         self._np_data = self._np_data[
             np.argsort(self._np_data, axis=self.DIM_INDEX)[:, self.DIM_INDEX]
@@ -114,7 +146,7 @@ class VehicleDataManager:
         """
         if not os.path.exists(self._file_path):
             self._has_error = True
-            print(f"Sorry, {self._file_path} not found")
+            self._logger.error(f"Sorry, file {self._file_path} not found")
 
     def _validate_filtered_segment(self) -> None:
         """
